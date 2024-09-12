b0dae2a747c5a71e27255fa3a0b09545ab64dc5d
Emma-Miller04 <littlepeanut0504@gmail.com>
2024-09-12 11:44:24 -0500

    Smooth Camera & Fix CamMovement

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -12,10 +12,10 @@ public class FollowPlayer : MonoBehaviour
 
 
     public GameObject player;
-    private Vector3 offset = new Vector3(0,5,-7);
+    private Vector3 offset = new Vector3(0,5,-10);
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = player.transform.position + offset;
     }
