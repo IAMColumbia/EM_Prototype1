ab468f6829a6ac0581f72436f35d70aed54fa793
Emma-Miller04 <littlepeanut0504@gmail.com>
2024-09-12 11:39:30 -0500

    Add Offset Variable

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -12,10 +12,11 @@ public class FollowPlayer : MonoBehaviour
 
 
     public GameObject player;
+    private Vector3 offset = new Vector3(0,5,-7);
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;
+        transform.position = player.transform.position + offset;
     }
 }
