ac30a822a16731a989480c60abb73b2d6245df37
Emma-Miller04 <littlepeanut0504@gmail.com>
2024-09-19 00:28:25 -0500

    Add Horizontal Turn

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 20;
+    public float turnSpeed;
+    public float horizontalInput;
+
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    public float speed = 20;
+    
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        
         transform.Translate(Vector3.forward * Time.deltaTime *speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
