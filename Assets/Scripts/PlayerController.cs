1df762d46a0a9164762f200e78eb8442f9bef9e1
Emma-Miller04 <littlepeanut0504@gmail.com>
2024-09-19 00:33:01 -0500

    Add Forward & Turn speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
-    public float turnSpeed;
-    public float horizontalInput;
+    private float speed = 20f;
+    private float turnSpeed = 45.0f;
+
+    private float horizontalInput;
+    private float forwardInput;
 
     void Start()
     {
@@ -16,8 +18,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         
-        transform.Translate(Vector3.forward * Time.deltaTime *speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        //moves car forward
+        transform.Translate(Vector3.forward * Time.deltaTime *speed * forwardInput);
+
+        //turns car horizontally
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
